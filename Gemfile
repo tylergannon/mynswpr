59259dd50fa06c187e2ed38e7101c24864af414a
Tyler Gannon <tgannon@gmail.com>
2013-03-19 16:24:58 -0700

    Initial Add

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.0.beta1'
+
+gem 'sqlite3'
+
+group :assets do
+  gem 'sass-rails',   '~> 4.0.0.beta1'
+  gem 'coffee-rails', '~> 4.0.0.beta1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+gem 'jasminerice', github: 'bradphelan/jasminerice'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MineSweeper::Application.config.secret_key_base = 'fc4709d47517b6b7ce453ae503ab8f7e08d9c0ed1ed3a5a0c20935d7dfe31bb79d54e2e3fece6b5499a4cb0f79978ee3fabe395fd9b79bf9a11eb03ad418f3b4'
