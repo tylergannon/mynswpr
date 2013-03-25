1cf6ceb97eab8e0558090d14c20f36f1a099f7a1
Tyler Gannon <tgannon@gmail.com>
2013-03-25 12:32:33 -0700

    Changed paths to reflect new nested folder hierarchy.

--- app/assets/javascripts/games/application.js
+++ app/assets/javascripts/games/application.js
@@ -10,9 +10,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require minesweeper/model
-//= require minesweeper/view
-//= require minesweeper/controller
+//= require ./minesweeper/model
+//= require ./minesweeper/view
+//= require ./minesweeper/controller
 //= require flash
-
-
